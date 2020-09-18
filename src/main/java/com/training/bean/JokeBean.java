38237d28a0d6c979448b004f7561968f67a59d1d
lei-long <leilong.5201>
2020-09-18 23:54:13 +0800

    修复joke数据上传接口错误问题

--- src/main/java/com/training/bean/JokeBean.java
+++ src/main/java/com/training/bean/JokeBean.java
@@ -13,6 +13,18 @@ public class JokeBean {
     private String source;
     private int assortId;
 
+    public JokeBean(String userId, String title, Timestamp postTime, String content, String source, int assortId) {
+        this.userId = userId;
+        this.title = title;
+        this.postTime = postTime;
+        this.content = content;
+        this.source = source;
+        this.assortId = assortId;
+    }
+
+    public JokeBean() {
+    }
+
     public int getAssortId() {
         return assortId;
     }
@@ -47,5 +59,19 @@ public class JokeBean {
 
     public Timestamp getPostTime() {
         return postTime;
+    }
+
+    @Override
+    public String toString() {
+        return "JokeBean{" +
+                "jokeId='" + jokeId + '\'' +
+                ", userId='" + userId + '\'' +
+                ", title='" + title + '\'' +
+                ", coverImg='" + coverImg + '\'' +
+                ", postTime=" + postTime +
+                ", content='" + content + '\'' +
+                ", source='" + source + '\'' +
+                ", assortId=" + assortId +
+                '}';
     }
 }
--- src/main/java/com/training/controller/JokeController.java
+++ src/main/java/com/training/controller/JokeController.java
@@ -4,15 +4,22 @@ import com.google.gson.Gson;
 import com.training.bean.JokeBean;
 import com.training.service.AssortService;
 import com.training.service.JokeService;
+import com.training.utils.IntactUtils;
+import com.training.utils.SpareData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.Random;
 
 @Controller
 @RequestMapping(value = "/joke")
@@ -39,10 +46,26 @@ public class JokeController {
 
     @RequestMapping(value = "/upload", method = RequestMethod.POST, produces = "application/json; charset=utf-8")
     @ResponseBody
-    public void saveJoke(@RequestParam("cover") MultipartFile file, @RequestBody JokeBean joke, HttpServletResponse response, HttpServletRequest request) throws IOException {
+    public void saveJoke(@RequestParam("cover") MultipartFile file, HttpServletResponse response, HttpServletRequest request) throws IOException {
         response.setContentType("text/html;charset=utf-8");
+
+        String userId = request.getParameter("userId");
+        String title = request.getParameter("title");
+        String post = request.getParameter("postTime");
+        String content = request.getParameter("content");
+        String source = request.getParameter("source");
+        String assort = request.getParameter("assortId");
+
+        if (!IntactUtils.isIntact(userId, title, post, content, content, source, assort)){
+            response.getWriter().println(new Gson().toJson(new SpareData<JokeBean>().failedByParameter()));
+        }
+        Timestamp postTime = new Timestamp(Long.parseLong(post));
+        int assortId = Integer.parseInt(assort);
+        JokeBean joke = new JokeBean(userId, title, postTime, content, source, assortId);
+
         String url = request.getSession().getServletContext().getRealPath("/img");
-        String filePath ="/joke/" + joke.getJokeId() + "_" + file.getOriginalFilename();//保存图
+
+        String filePath ="/joke/" + joke.getUserId() + "_" + String.format("%04s", new Random().nextInt(9999)) + file.getOriginalFilename();//保存图
         file.transferTo(new File(url + filePath));
         joke.setCoverImg("img" + filePath);
         response.getWriter().println(new Gson().toJson(service.saveJoke(joke)));
@@ -56,10 +79,10 @@ public class JokeController {
     }
 
     //删除新闻
-    @RequestMapping(value = "delete")
-    public void deleteJoke(HttpServletResponse response, HttpServletRequest request){
+    @RequestMapping(value = "/delete")
+    public void deleteJoke(HttpServletResponse response, HttpServletRequest request) throws IOException {
         String jokeId = request.getParameter("jokeId");
         String userId = request.getParameter("userId");
-
+        response.getWriter().println(new Gson().toJson(service.deleteJokeById(userId, jokeId)));
     }
 }
